8a494368685014bd6d3bb85da04bfae34e202474
cwesteri <constantine.westerink@gmail.com>
2025-03-11 15:56:32 -0400

    Fix configs

--- server/src/db/queries.ts
+++ server/src/db/queries.ts
@@ -1,22 +1,23 @@
 // import pool from '../db/db'
 import { Request, Response } from 'express';
-import prisma from '../prismaClient.js';
+
+import prisma from '../prisma/prismaClient';
 
 export const getTrails = async (req: Request, res: Response) => {
     try {
         const trails = await prisma.trails.findMany();
-        res.status(200).json(trails)
+        res.status(200).json(trails);
     } catch(err: unknown) {
         if (err instanceof Error) {
             console.error(err.message);
-            res.status(500).json({ error: "Server Error"});
+            res.status(500).json({ error: 'Server Error' });
         } else {
             console.error('An unknown error has occurred');
             res.status(500).send('Unknown server error');
         }
         
     }
-}
+};
 
 export const getUsers = async (req: Request, res: Response) => {
     try {
@@ -24,8 +25,7 @@ export const getUsers = async (req: Request, res: Response) => {
         res.status(200).json(users);
     } catch (err) {
         console.error(err);
-        res.status(500).json({ error: "Server Error" });
+        res.status(500).json({ error: 'Server Error' });
     }
 };
-
 
