f5c0b678629e6014021cff01eef3a80928260acc
cwesteri <constantine.westerink@gmail.com>
2025-03-14 03:09:22 -0400

    Setup Google Cloud Storage

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,17 @@
 import { app } from './app';
 
+import { initStorage } from '@/config/storage';
+
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-    // eslint-disable-next-line no-console
-    console.log(`Server running on port ${port}`);
-});
+async function startServer() {
+    // Execute any async setup before running server
+    await initStorage();
+
+    app.listen(port, () => {
+        // eslint-disable-next-line no-console
+        console.log(`Server running on port ${port}`);
+    });
+};
+
+startServer();
