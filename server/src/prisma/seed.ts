ef23553e142d71989ae9f35351ade7d61062735d
vickyc2266 <146585559+vickyc2266@users.noreply.github.com>
2025-03-18 14:03:51 -0400

    attempted to fix lint issue

--- server/src/prisma/seed.ts
+++ server/src/prisma/seed.ts
@@ -6,16 +6,16 @@ async function main() {
     
     const park1 = await prisma.parks.create({
         data: {
-            name: "Grand Canyon National Park", 
-            county: "Coconino",
+            name: 'Grand Canyon National Park', 
+            county: 'Coconino',
             is_active: true
         }
     });
 
     const park2 = await prisma.parks.create({
         data: {
-            name: "Yellowstone National Park",
-            county: "Teton",
+            name: 'Yellowstone National Park',
+            county: 'Teton',
             is_active:true
         }
     });
@@ -24,19 +24,19 @@ async function main() {
         data: [
             {
                 park_id: park1.park_id, 
-                name: "Great Allegheny Passage",
+                name: 'Great Allegheny Passage',
                 is_active: true, 
                 is_open: true
             },
             {
                 park_id: park1.park_id,
-                name: "Three Rivers Heritage Trail",
+                name: 'Three Rivers Heritage Trail',
                 is_active: true,
                 is_open: false
             },
             {
                 park_id: park2.park_id,
-                name: "Vicky's Wonderland",
+                name: 'Vicky\'s Wonderland',
                 is_active: true,
                 is_open: false
             }
