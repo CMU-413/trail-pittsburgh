2b54543b1ac0d98c6441977003329971c14e033b
cwesteri <constantine.westerink@gmail.com>
2025-04-09 18:24:52 -0400

    lint bootstrap file

--- server/tsconfig-paths-bootstrap.js
+++ server/tsconfig-paths-bootstrap.js
@@ -1,8 +1,9 @@
 const tsConfigPaths = require('tsconfig-paths');
+
 const tsConfig = require('./tsconfig.json');
 
 const baseUrl = './dist'; // This is where your compiled files are
 const cleanup = tsConfigPaths.register({
-  baseUrl,
-  paths: { '@/*': ['./*'] } // This maps @/* to the dist directory
-}); 
+    baseUrl,
+    paths: { '@/*': ['./*'] } // This maps @/* to the dist directory
+});
