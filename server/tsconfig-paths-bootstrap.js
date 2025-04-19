0dc11d0b27a3f083b0ffff851b27e2e7e973b676
cwesteri <constantine.westerink@gmail.com>
2025-04-19 09:35:39 -0400

    Fix config for build

--- server/tsconfig-paths-bootstrap.js
+++ server/tsconfig-paths-bootstrap.js
@@ -1,9 +1,8 @@
-const tsConfigPaths = require('tsconfig-paths');
+import tsConfigPaths from 'tsconfig-paths';
 
-const tsConfig = require('./tsconfig.json');
-
-const baseUrl = './dist'; // This is where your compiled files are
-const cleanup = tsConfigPaths.register({
-    baseUrl,
-    paths: { '@/*': ['./*'] } // This maps @/* to the dist directory
+tsConfigPaths.register({
+    baseUrl: './dist',
+    paths: {
+        '@/*': ['*'],
+    },
 });
