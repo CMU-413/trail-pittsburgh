becc889134a418e755bf202e9f2222a1a10e65d1
cwesteri <constantine.westerink@gmail.com>
2025-04-22 00:05:19 -0400

    Set cors origin to client url based on env

--- server/src/app.ts
+++ server/src/app.ts
@@ -21,7 +21,7 @@ app.use(securityHeaders);
 
 // CORS configuration
 app.use(cors({
-    origin: 'https://trail-pgh-issue-tracker.web.app',
+    origin: process.env.CLIENT_URL,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
