79cb4f4e27b348dd70c9bc6ca0e1ad9cb7844706
cwesteri <constantine.westerink@gmail.com>
2025-04-25 13:19:05 -0400

    Restructure GCSBucket to allow DI

--- server/src/lib/GCSBucket.ts
+++ server/src/lib/GCSBucket.ts
@@ -1,6 +1,26 @@
 import {
-    GetSignedUrlConfig, Bucket, Storage 
+    GetSignedUrlConfig, Bucket, Storage
 } from '@google-cloud/storage';
+
+export function createBucket(bucketName: string): Bucket {
+    const isTest = process.env.NODE_ENV === 'test';
+
+    if (isTest) {
+        throw new Error('Bucket creation is disabled in test mode.');
+    }
+
+    let storage;
+    if (process.env.NODE_ENV === 'production') {
+        storage = new Storage();
+    } else {
+        const keyFilename = process.env.GCS_SERVICE_ACCOUNT_KEY_FILENAME;
+        if (!keyFilename) {
+            throw new Error('Service account key required to access google cloud storage');
+        }
+        storage = new Storage({ keyFilename });
+    }
+    return storage.bucket(bucketName);
+}
 
 export type SignedUrl = {
     key: string;
@@ -14,9 +34,8 @@ export class GCSBucket {
     private static readonly UPLOAD_BUCKET_TIMEOUT: number = 20;
     private static readonly DOWNLOAD_BUCKET_TIMEOUT: number = 5 * 60;
 
-    public constructor(bucketName: string) {
-        const storage = new Storage();
-        this.bucket = storage.bucket(bucketName);
+    public constructor(bucket: Bucket) {
+        this.bucket = bucket;
     }
 
     async getDownloadUrl(key: string): Promise<SignedUrl> {
