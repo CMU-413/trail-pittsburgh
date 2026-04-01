05079a5284552eede497f1964441f1bd1f141e63
Alondra <arobles@andrew.cmu.edu>
2026-03-31 21:08:18 -0400

    Add useAuth import in IssueReportForm.tsx

--- client/src/components/issues/IssueReportForm.tsx
+++ client/src/components/issues/IssueReportForm.tsx
@@ -14,6 +14,7 @@ import { TextArea } from '../ui/TextArea';
 import { parkApi } from '../../services/api';
 import { getParkByLatLng } from '../../utils/parkUtils';
 import { Select } from '../ui/Select';
+import { useAuth } from '../../providers/AuthProvider';
 
 interface IssueReportFormProps {
     onSubmit: (data: IssueParams) => Promise<void>;
